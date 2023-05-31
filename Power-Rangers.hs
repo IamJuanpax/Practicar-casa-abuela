b86f34dd5ce2a7b9761ac164a6ec337e099291b3
Juanpa Britos <jubritos@frba.utn.edu.ar>
2023-05-31 18:16:24 -0300

    agregue funciones para programar y el punto 4 a

--- Power-Rangers.hs
+++ Power-Rangers.hs
@@ -17,6 +17,8 @@ juanpa = UnaPersona "Juan Pablo" ["Valiente","Veloz"] True
 
 powerRojo = UnPowerRanger "rojo" ["Crecer","Invisibilidad"] 20
 
+---- Punto 2 -----
+
 habilidadesSuper :: Persona -> [String]
 habilidadesSuper persona = map (++"Super") (habilidades persona)
 
@@ -26,9 +28,38 @@ sumarTamañoPalabra persona = sum . map length . habilidades $ persona
 convertirEnPowerRanger :: String -> Persona -> PowerRanger
 convertirEnPowerRanger colorDado persona = UnPowerRanger {color=colorDado, habilidadesPower= habilidadesSuper persona, nivelPelea= sumarTamañoPalabra persona}
 
+------ Punto 3 ------
+
 personaBuena :: Persona -> Bool
 personaBuena persona = buena persona   -- suponemos que esta bien
 
 formarEquipoRanger :: [String] -> [Persona] -> [PowerRanger]
 formarEquipoRanger colores personas = map convertirEnPowerRanger (filter personaBuena personas)
 
+---- Punto 4 a) ------
+
+findOrElse :: (a -> Bool) -> a -> [a] -> a
+findOrElse funcion valor lista
+    | find funcion lista \= null = find funcion lista     -- suponiendo que exista null
+    | otherwise = valor
+
+---- Punto 4 b) ----
+
+rangerLider ::
+rangerLider 
+
+---- Punto 5 a) ----
+
+maximumBy ::
+maximumBy
+
+---- Punto 5 b) ----
+
+rangerMásPoderoso ::
+rangerMásPoderoso
+
+
+----- Punto 6 ------
+
+rangerHabilidoso ::
+rangerHabilidoso
